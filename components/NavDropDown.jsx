ae27aae641c13065fa7c7c7b1dd7f119b6403480
Lienkulet <clocicovalexandru@gmail.com>
2023-04-05 22:32:48 +0300

    navbar mobile fix

--- components/NavDropDown.jsx
+++ components/NavDropDown.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
+import { useStateContext } from '@/context/StateContext';
 
 const NavDropDown = () => {
+
+  const { setShowMenu } = useStateContext();
+
   return (
-    <div>NavDropDown</div>
+    <div className="sectionsMobile">
+        <Link href='#about' className='section' onClick={() => setShowMenu(0)}>
+          About
+        </Link>
+        <Link href='#skills' className='section' onClick={() => setShowMenu(0)}>
+          Skills
+        </Link>
+        <Link href='#work' className='section' onClick={() => setShowMenu(0)}>
+          Work
+        </Link>
+        <Link href='#contact' className='section' onClick={() => setShowMenu(0)}>
+          Contact
+        </Link>
+      </div>
   )
 }
 
